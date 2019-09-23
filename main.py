b454975141212ce1e23d26685d1affbce2454d04
Aaronmac64 <aaronmacn64@gmail.com>
2019-09-23 14:27:53 -0500

    Finished basically all html code, added most python code. Too much for a single commit honestly

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 import cgi
 
 app = Flask(__name__)
@@ -8,7 +8,16 @@ app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 @app.route("/")
 def index():
 
-    content = "asdf"
-    return content
+    return render_template('submission_form.html',)
+
+@app.route("/result", methods=['POST'])
+def resultform():
+
+    username = request.form['username']
+    password = request.form['password']
+    verify_password = request.form['verify_password']
+    email = request.form['email']
+
+    return render_template('result_form.html',username=username)
 
 app.run()
